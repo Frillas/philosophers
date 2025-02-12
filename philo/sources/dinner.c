de9ce2f38c9f366141bf9584e51fce7852cf1fed
frillas <roulleau.anthonin@orange.fr>
2025-02-12 14:02:01 +0100

    having fun with threads

--- philo/sources/dinner.c
+++ philo/sources/dinner.c
@@ -6,7 +6,7 @@
 /*   By: aroullea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 16:08:37 by aroullea          #+#    #+#             */
-/*   Updated: 2025/02/12 12:10:00 by aroullea         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:51:50 by aroullea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,6 +82,13 @@ int	start_dinner(t_rules *dinning_rules, t_philo *philo, pthread_t *thread_id)
 	{
 		if (pthread_join(thread_id[i], NULL) != 0)
 			return (EXIT_FAILURE);
+		current = current->right;
+		i++;
+	}
+	i = 0;
+	current = philo;
+	while (i < dinning_rules->nb_philo)
+	{
 		pthread_mutex_destroy(&current->mutex);
 		current = current->right;
 		i++;
