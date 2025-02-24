08939ff5f391463567cfe2a8b6c7474d12ecef1d
frillas <roulleau.anthonin@orange.fr>
2025-02-24 20:57:28 +0100

    bonus done/ready to push

--- philo/sources/threads/threads_error.c
+++ philo/sources/threads/threads_error.c
@@ -6,7 +6,7 @@
 /*   By: aroullea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:45:48 by aroullea          #+#    #+#             */
-/*   Updated: 2025/02/24 20:39:26 by aroullea         ###   ########.fr       */
+/*   Updated: 2025/02/24 20:56:37 by aroullea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	error(t_philo *philo, pthread_t *thread, pthread_t *moni, long tot)
 
 	nb_thread_waited = 0;
 	current = philo;
-	while (nb_thread_waited < created - 1)
+	while (nb_thread_waited < tot - 1)
 	{
 		if (pthread_join(thread[nb_thread_waited], NULL) != 0)
 			write(2, "thread wait error\n", 18);
@@ -52,9 +52,9 @@ void	err_thread(t_philo *philo, pthread_t *t_id, pthread_t *moni, long tot)
 {
 	t_rules	*dining_rules;
 
-	if (created == 0)
+	if (tot == 0)
 		return ;
 	dining_rules = philo->lst_rules;
-	error(philo, t_id, moni, created);
+	error(philo, t_id, moni, tot);
 	err_destroy(philo, dining_rules);
 }
